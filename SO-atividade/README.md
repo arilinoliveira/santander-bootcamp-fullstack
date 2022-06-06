002bc080be5cf602a46aa5e0db49e45cd78d8763
arilinoliveira <arilin.oliveira@gmail.com>
2022-06-05 21:19:15 -0300

    add SO-atividade

---
+++ SO-atividade/README.md
@@ -0,0 +1,18 @@
+# Sintaxe e Operadores (JavaScript) :trophy:
+Atividade prática da aula Sintaxe e Operadores, do Módulo II - HTML/CSS/JS, do Santander Bootcamp Fullstack Developer.
+
+## Objetivos
+ - Criar uma função que recebe dois números como parâmetros;
+ - Conferir se os números são iguais;
+ - Conferir se a soma dos números é maior que 10 ou menor que 20;
+ - Retornar uma string dizendo "Os números num1 e num2 não/são iguais. Sua soma é soma, que é maior/menor que 10 e maior/menor que 20".
+
+ ## Considerações
+Deu um certo trabalho, já que ainda não tenho tanta habilidade com JavaScript no momento, mas fiquei muito feliz em conseguir resolver o problema sem consultar a solução da professora. Também consegui ir além, colocando mais duas condicionais:
+
+ - Conferir se a soma dos números é igual a 10;
+ - Conferir se a soma dos números é igual a 20.
+
+Acho que finalmente estou começando a me entender com essa linguagem, haha.
+
+Até a próxima! :wave:
---
+++ SO-atividade/sintaxeoperadores.js
@@ -0,0 +1,38 @@
+var num1 = 0;
+var num2 = 0;
+var soma = 0;
+
+
+function equal(num1, num2){
+    if (num1 === num2){
+        return "Os números " + num1 + " e " + num2 + " são iguais. ";
+    } else {
+        return "Os números " + num1 + " e " + num2 + " não são iguais. ";
+    }
+}
+
+function biggerSmaller(num1, num2){
+    let soma = num1 + num2;
+
+    if (soma < 10){
+        return "Sua soma é " + soma + ", que é menor que 10 e menor que 20.";
+    }
+    if (soma === 10){
+        return "Sua soma é " + soma + ", que é igual a 10 e menor que 20.";
+    }
+    if (soma === 20){
+        return "Sua soma é " + soma + ", que é maior que 10 e igual a 20.";
+    }
+    if (10 < soma < 20){
+        return "Sua soma é " + soma + ", que é maior que 10 e menor que 20.";
+    }
+    if (soma > 20){
+        return "Sua soma é " + soma + ", que é maior que 10 e maior que 20.";
+    }
+}
+
+function result(num1, num2){
+    return equal(num1, num2) + biggerSmaller(num1, num2);
+}
+
+console.log(result(1, 2));
